9b2b5b5440237ceb736244d3172e48629e3e939b
Dellayulischa <dellayulischa@gmail.com>
2018-07-18 06:40:29 +0700

    Add files via upload

---
+++ bando.js
@@ -0,0 +1,6 @@
+.related-post-style-2 .related-post-item-title {color: #333;}
+.related-post-style-2 .related-post-item-title {font-size:16px;font-weight:500;margin:0;padding:10px 0;line-height:1.2;}
+.related-post-style-2 .related-post-item-thumbnail img {width:100%;height:100%}
+.related-post-style-2 .related-post-item-thumbnail {padding:10px;width:25%;height:100%;float:left;position:relative;margin-right:5px;transition:all .3s ease-in-out}
+.related-post-style-2 li {width: 50%;}
+.related-post-style-2 li {display: block;padding: 0;float: left;width: 20%;height: 210px;overflow: hidden;margin: 0 0 4px;padding: 0 4px 0 0;list-style: none;}
